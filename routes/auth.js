63272823d1f739092811fec2767d54c2ffe6e038
Peter Farr <Peter@PrismaPhonic.com>
2018-11-02 22:22:03 -0700

    added /login auth route

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/user');
+const { classPartialUpdate } = require('../helpers/partialUpdate');
+const jwt = require('jsonwebtoken');
+const { SECRET } = require('../config');
+
+// Login a user
+router.post('/login', async function (req, res, next) {
+  try {
+    const token = await User.authenticate(req.body);
+    return res.json({ token });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = router;
