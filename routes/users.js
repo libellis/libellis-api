7e28066cfe3b6a53664af69e84c391af778f0b9d
Peter Farr <Peter@PrismaPhonic.com>
2018-11-03 10:22:30 -0700

    added middleware paths in user routes for json schema validation when making a new user, or editing an existing user

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ const User = require('../models/user');
 // const Survey = require('../models/survey');
 // const Question = require('../models/question');
 const { classPartialUpdate } = require('../helpers/partialUpdate');
+const validateInput = require('../middleware/validation');
+const newUserSchema = require('../schema/newUser.json');
+const updateUserSchema = require('../schema/updateUser.json');
 const { ensureLoggedIn, ensureCorrectUser } = require('../middleware/auth');
 
 //Get a list of users
@@ -17,7 +20,7 @@ router.get('/', async function (req, res, next) {
 });
 
 //Create a new user
-router.post('/', async function (req, res, next) {
+router.post('/', validateInput(newUserSchema), async function (req, res, next) {
   try {
     await User.createUser(req.body);
     const token = await User.authenticate(req.body);
@@ -28,7 +31,7 @@ router.post('/', async function (req, res, next) {
 });
 
 //Get a user by username
-router.get('/:username', async function (req, res, next) {
+router.get('/:username', ensureCorrectUser, async function (req, res, next) {
   try {
     const user = await User.getUser(req.params.username);
     return res.json({ user });
@@ -38,16 +41,20 @@ router.get('/:username', async function (req, res, next) {
 });
 
 //Update a user
-router.patch('/:username', ensureCorrectUser, async function (req, res, next) {
-  try {
-    let user = await User.getUser(req.params.username);
-    user.updateFromValues(req.body);
-    await user.save();
-    return res.json({ user });
-  } catch (error) {
-    return next(error);
+router.patch(
+  '/:username',
+  ensureCorrectUser,
+  validateInput(updateUserSchema),
+  async function (req, res, next) {
+    try {
+      let user = await User.getUser(req.params.username);
+      user.updateFromValues(req.body);
+      await user.save();
+      return res.json({ user });
+    } catch (error) {
+      return next(error);
+    }
   }
-}
 );
 
 //Delete a user
