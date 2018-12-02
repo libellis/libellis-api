367606cd7e516a03ef5922fb4381e771a5d2c2e2
Kevin Welch <kaedub16@gmail.com>
2018-12-01 21:43:20 -0800

    Added a route to return a user's voting history as a list of surveys ids that a user has voted on at /users/:username/history

--- __tests__/integration/votes.test.js
+++ __tests__/integration/votes.test.js
@@ -241,7 +241,7 @@ describe('POST /surveys/:survey_id/votes', () => {
     });
 
     expect(response.status).toEqual(400);
-    expect(response.body.message).toEqual("Either those quetions or choices do not exist in survey with id: 1");
+    expect(response.body.message).toEqual("Either those questions or choices do not exist in survey with id: 1");
   });
 });
 
