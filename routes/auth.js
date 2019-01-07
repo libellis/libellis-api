59c09c0a03ca6008779ff1cc652ea04695b799a4
Kevin Welch <kaedub16@gmail.com>
2019-01-06 23:39:21 -0800

    Sign up now validates with newUserSchema

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
+const validateInput = require('../middleware/validation');
+const newUserSchema = require('../schema/newUser.json');
 const { classPartialUpdate } = require('../helpers/partialUpdate');
 const jwt = require('jsonwebtoken');
 const { SECRET } = require('../config');
@@ -16,12 +18,13 @@ router.post('/login', async function (req, res, next) {
   }
 });
 
-router.post('/signup', async function (req, res, next) {
+router.post('/signup', validateInput(newUserSchema), async function (req, res, next) {
   try {
     console.log('received', req.body);
     await User.createUser(req.body);
     const token = await User.authenticate(req.body);
     return res.json({ token });
+    // return res.json("User created")
   } catch (error) {
     return next(error);
   }
