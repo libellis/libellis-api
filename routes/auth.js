59bfd4e633185b56d301f046fc6cbc1335157ed9
Kevin Welch <kaedub16@gmail.com>
2019-01-06 23:40:47 -0800

    Removed unused imports and commented out code

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,6 @@ const router = new express.Router();
 const User = require('../models/user');
 const validateInput = require('../middleware/validation');
 const newUserSchema = require('../schema/newUser.json');
-const { classPartialUpdate } = require('../helpers/partialUpdate');
-const jwt = require('jsonwebtoken');
-const { SECRET } = require('../config');
 
 // Login a user
 router.post('/login', async function (req, res, next) {
@@ -20,11 +17,9 @@ router.post('/login', async function (req, res, next) {
 
 router.post('/signup', validateInput(newUserSchema), async function (req, res, next) {
   try {
-    console.log('received', req.body);
     await User.createUser(req.body);
     const token = await User.authenticate(req.body);
     return res.json({ token });
-    // return res.json("User created")
   } catch (error) {
     return next(error);
   }
