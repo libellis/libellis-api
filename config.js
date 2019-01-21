03909d4fddccc339d78566c97e93743581cf9281
Peter Farr <Peter@PrismaPhonic.com>
2019-01-21 12:31:04 -0800

    Updated test database to use underscore due to travis build restrictions

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const PORT = +process.env.PORT || 5000;
 let DB_URI;
 
 if (process.env.NODE_ENV === 'test') {
-  DB_URI = 'libellis-test';
+  DB_URI = 'libellis_test';
 } else {
   DB_URI = process.env.DATABASE_URL || 'libellis';
 }
