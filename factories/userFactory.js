8efaff918be00712cad718e1d14f9d8eecaf90af
Kevin Welch <kaedub16@gmail.com>
2019-02-24 17:12:13 -0800

    Created a user factory function for test setup

---
+++ factories/userFactory.js
@@ -0,0 +1,18 @@
+async function userFactory(data) {
+  const { username, password, first_name, last_name, email, is_admin } = data;
+
+  const user = await User.createUser({
+    username,
+    password,
+    first_name,
+    last_name,
+    email,
+    is_admin
+  });
+
+  user.token = await User.authenticate({ username, password });
+
+  return user;
+}
+
+async;
