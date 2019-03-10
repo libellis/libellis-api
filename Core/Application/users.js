5f73c40bbd9a188999d7fb853c2b48f49aa9272b
Peter Farr <Peter@PrismaPhonic.com>
2019-03-10 13:22:00 -0700

    Worked through a good portion of users application

--- Core/Application/users.js
+++ Core/Application/users.js
@@ -1,1 +1,23 @@
+const { UnitOfWork } = require('../../Persistence/Repositories/UnitOfWork');
+const { User } = require('../Domain/user');
 
+async function getAllUsers({ db }) {
+    const unitOfWork = new UnitOfWork(db);
+    const users = unitOfWork.users.getAll();
+    return JSON.stringify({ users });
+}
+
+async function getUser({ db }, username){
+    const unitOfWork = new UnitOfWork(db);
+    const user = unitOfWork.users.get({ username });
+    return JSON.stringify({ user });
+}
+
+async function createUser({ db }, userData){
+    const unitOfWork = new UnitOfWork(db);
+    
+    const newUser = new User()
+    
+    const user = unitOfWork.users.get({ username });
+    return JSON.stringify({ user });
+}
